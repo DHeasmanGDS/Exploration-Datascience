fa2e850bba6c2859ec2910dbb6ccd064e9b42925
Heasman <heasman.drew@gmail.com>
2018-12-19 13:18:52 -0330

    Completed initial random number generator for x,y,z coordinates.

--- synth-data.py
+++ synth-data.py
@@ -1,9 +1,24 @@
-#Create a random data set.
+# Create a random data set and writes to a file.
+# Author: Drew Heasman
+# Date: December 19, 2018
+# Future implimentation:    Explode high value instances of uranium along elongated, "fault-like" directions.
+#                           Add a sandstone/basement value (binary) (possibly just a set value with no randomness
+#                           Add illite plumes to sandstone.
+#                           Create fault like areas with no uranium values (perhaps a graphite value)
 
 import random
-import numPy as np
 
+U = []          #initialize the random data list.
+uran = 0        #initialize the uranium value
 
-for i in range(1000):
+for x in range(0, 100):                                 #loop to create a random data set with x,y, and z coord
+    for y in range(0, 100):
+        for z in range(0, 100):
+            uran = random.randint(-1500000, 50000)      #set the statistical variance of high value uranium
+            if uran < 100:                              #set the base limit of uranium values
+                uran = 100
+            U.append([x, y, z, uran])                   #append the uranium value to the x, y, z, coord in the list
 
-    print(random.randrange(-150000,50000))
+with open('test_data.csv', 'w') as f:                   #write the entire list to a file
+    for item in U:
+        f.write("%s\n" % item)
