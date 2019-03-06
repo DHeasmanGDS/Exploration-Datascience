189c8241217b6fca9afc8b08f80589ea42a9d562
Drew Heasman <45907989+drew026@users.noreply.github.com>
2019-03-06 18:46:23 -0500

    took out inefficient for loops
    replace at later date with pandas dataframe, or numpy arrays.

--- synth-data.py
+++ synth-data.py
@@ -11,14 +11,6 @@ import random
 U = []          #initialize the random data list.
 uran = 0        #initialize the uranium value
 
-for x in range(0, 100):                                 #loop to create a random data set with x,y, and z coord
-    for y in range(0, 100):
-        for z in range(0, 100):
-            uran = random.randint(-1500000, 50000)      #set the statistical variance of high value uranium
-            if uran < 100:                              #set the base limit of uranium values
-                uran = 100
-            U.append([x, y, z, uran])                   #append the uranium value to the x, y, z, coord in the list
-
 with open('test_data.csv', 'w') as f:                   #write the entire list to a file
     for item in U:
         f.write("%s\n" % item)
